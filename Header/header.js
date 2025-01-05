c97cd50ebeb0e4fcb5eaad66b2e575a5521459a1
Kawya Awarjana <kawya.evotech@gmail.com>
2025-01-06 01:45:24 +0530

    Kawya

--- Header/header.js
+++ Header/header.js
@@ -15,3 +15,30 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+
+// Apply scroll effect to all headers
+window.addEventListener("scroll", function () {
+  const headers = document.querySelectorAll(".header--sec1--div1, .header--sec2--div1, .header--sec3--div1"); // Select all headers
+
+  headers.forEach(header => {
+    if (window.scrollY > 0) {
+      header.style.backgroundColor = "white";
+      header.style.color = "black";
+
+      // Update text colors within the header
+      header.querySelectorAll(".header--text,.header--text3, .dropbtn").forEach(element => {
+        element.style.color = "black";
+      });
+    } else {
+      header.style.backgroundColor = "transparent";
+      header.style.color = "white";
+
+      // Revert text colors within the header
+      header.querySelectorAll(".header--text,.header--text3, .dropbtn").forEach(element => {
+        element.style.color = "white";
+      });
+    }
+  });
+});
